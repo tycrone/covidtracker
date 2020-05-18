7854e623ef5a1c3f491f06106b180b6bb694cc5f
Tyler Crone <tcrone23@hotmail.com>
2020-05-17 22:39:46 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 
+// import Cards from './components/Cards/Cards';
+// import Chart from './components/Chart/Chart';
+// import Cards from './components/Cards/Cards';
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
+import { fetchData } from './api';
+
 class App extends React.Component{
+
+	async componentDidMount(){
+		const data = await fetchData();
+		console.log(data);
+	}
+
 	render(){
 		return(
-			<div>
-				<h1>App</h1>
+			<div className={styles.container}>
+				<Cards />
+				<CountryPicker />
+				<Chart />
 			</div>
 		)
 	}
